f81754dbacab6ecb0e34b6b1d296a3775968412f
Lucas Martinez - Atividade da aula 4 <lucmartinez09@gmail.com>
2026-07-01 18:38:45 -0300

    Initial commit

---
+++ atividade1.js
@@ -0,0 +1,14 @@
+let saldo = 200;
+let saque = 150;
+
+try {
+    if (saque > saldo) {
+        throw new Error("Saldo insuficiente para realizar o saque.");
+    }
+
+    saldo = saldo - saque;
+    console.log("Saque realizado com sucesso!");
+    console.log("Saldo atual: R$ " + saldo);
+} catch (erro) {
+    console.log("Erro: " + erro.message);
+}
