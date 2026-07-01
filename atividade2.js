b2bdfd1b0e4c38b3b1002d5bd771fdfcebe26327
Lucas Martinez - Atividade da aula 4 <lucmartinez09@gmail.com>
2026-07-01 20:56:30 -0300

    inclusão e correção
    Incluindo a atividade 3 e corrigindo a atividade 2

--- atividade2.js
+++ atividade2.js
@@ -1,5 +1,11 @@
-let Numero = 9;
+/*let Numero = 9;
 
 for( let i = 1; i<=10; i++){
     console.log(Numero, "x", i, "=", Numero * i);
+}*/
+
+for (let i = 1; i <= 10; i++){
+    for (let j = 1; j <= 10; j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
 }
---
+++ atividade3.js
@@ -0,0 +1,7 @@
+let valor = 123;
+
+for (let i = 0; i <= valor; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
